bac33f25fbc789dc762efc3165f97234fa99fe21
satishusk <sanpavkaz@gmail.com>
2023-03-01 19:17:48 +0400

    Added get all products method

--- backend/store/product-service/src/main/java/com/team/productservice/service/ProductService.java
+++ backend/store/product-service/src/main/java/com/team/productservice/service/ProductService.java
@@ -1,5 +1,10 @@
 package com.team.productservice.service;
 
+import com.team.productservice.data.Product;
+
+import java.util.List;
+
 public interface ProductService {
   void seedData();
+  List<Product> getAll();
 }
