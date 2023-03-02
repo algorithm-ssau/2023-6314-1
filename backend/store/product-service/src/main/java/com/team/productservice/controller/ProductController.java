c32534b3aa308bb4dc4fb6f67459996b5f7c1453
satishusk <sanpavkaz@gmail.com>
2023-03-02 23:27:06 +0400

    Deleted uselessness endpoint /api/products/seeddata

--- backend/store/product-service/src/main/java/com/team/productservice/controller/ProductController.java
+++ backend/store/product-service/src/main/java/com/team/productservice/controller/ProductController.java
@@ -1,16 +1,11 @@
 package com.team.productservice.controller;
 
-import com.team.productservice.data.Product;
 import com.team.productservice.dto.ProductResponseDto;
 import com.team.productservice.service.ProductService;
 import com.team.productservice.service.mapper.ProductMapper;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,13 +15,6 @@ import java.util.List;
 public class ProductController {
   private final ProductService productService;
   private final ProductMapper productMapper;
-
-
-  @PostMapping("/api/products/seeddata")
-  public ResponseEntity<Product> seedData() {
-    productService.seedData();
-    return ResponseEntity.status(HttpStatusCode.valueOf(204)).build();
-  }
 
   @GetMapping("/api/products")
   public ResponseEntity<List<ProductResponseDto>> getAll() {
