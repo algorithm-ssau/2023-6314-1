7763e983627009d16031a9daebdb923d20c98501
satishusk <sanpavkaz@gmail.com>
2023-03-03 00:11:21 +0400

    Deleted useless autowired

--- backend/store/product-service/src/main/java/com/team/productservice/service/startup/strategy/SetupStrategy.java
+++ backend/store/product-service/src/main/java/com/team/productservice/service/startup/strategy/SetupStrategy.java
@@ -1,12 +1,10 @@
 package com.team.productservice.service.startup.strategy;
 
 import com.team.productservice.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class SetupStrategy {
   protected final ProductRepository productRepository;
 
-  @Autowired
   public SetupStrategy(ProductRepository productRepository) {
     this.productRepository = productRepository;
   }
