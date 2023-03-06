a21bae4d9d956e0217b160908ae048da70324d2b
evnikitin <yevgeniy_n12@mail.ru>
2023-03-06 12:48:43 +0400

    create home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
+
 import './App.css';
-import data from './data';
-import {BrowserRouter} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
@@ -10,25 +11,9 @@ function App() {
           <a href="/">Online Shop</a>
         </header>
         <main>
-        <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>}/>
+          </Routes>        
         </main>
       </div>
     </BrowserRouter>
