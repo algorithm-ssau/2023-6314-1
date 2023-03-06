35604e016c7b49a059dab0aaf5f2b3cd8fd62cad
evnikitin <yevgeniy_n12@mail.ru>
2023-03-06 12:36:02 +0400

    started implementing routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,37 @@
 import './App.css';
 import data from './data';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      <header>
-        <a href="/">Online Shop</a>
-      </header>
-      <main>
-      <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
+    <BrowserRouter>
+      <div >
+        <header>
+          <a href="/">Online Shop</a>
+        </header>
+        <main>
+        <h1>Featured Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
                 <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
+                  <img src={product.image} alt={product.name} />
                 </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
