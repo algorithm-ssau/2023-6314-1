f838b24b6c4d9093a8f1e4c48093ff54358169b5
satishusk <sanpavkaz@gmail.com>
2023-03-06 22:20:28 +0400

    Added order service

---
+++ backend/store/order-service/src/main/java/com/team/orderservice/service/impl/CommonOrderService.java
@@ -0,0 +1,42 @@
+package com.team.orderservice.service.impl;
+
+import com.team.orderservice.data.Order;
+import com.team.orderservice.exception.OrderNotFoundException;
+import com.team.orderservice.repository.OrderRepository;
+import com.team.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommonOrderService implements OrderService {
+  private final OrderRepository orderRepository;
+
+  @Override
+  public List<Order> getAll() {
+    return orderRepository.findAll();
+  }
+
+  @Override
+  public void create(Order order) {
+    orderRepository.save(order);
+  }
+
+  @Override
+  public List<Order> getAllByUserId(Long userId) {
+    return orderRepository.findAllByUserId(userId);
+  }
+
+  @Override
+  public Order getById(Long id) {
+    return orderRepository.findById(id)
+      .orElseThrow(() -> new OrderNotFoundException("Order with id: " + id + " not found"));
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    orderRepository.deleteById(id);
+  }
+}
