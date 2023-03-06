ffcd3937e367a1fb1b3b18c8fea3dde25865e8bb
satishusk <sanpavkaz@gmail.com>
2023-03-06 21:00:27 +0400

    Restriction of the relevance of orders for end-to-end logic has been changed
    public @interface Where
    Specifies a restriction written in native SQL to add to the generated SQL when querying an entity or collection.
    For example, @Where("deleted = false") could be used to hide entity instances which have been soft-deleted.
    Note that Where restrictions are always applied and cannot be disabled. They're therefore much less flexible than filters.

--- backend/store/order-service/src/main/java/com/team/orderservice/repository/OrderRepository.java
+++ backend/store/order-service/src/main/java/com/team/orderservice/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.team.orderservice.repository;
 
 import com.team.orderservice.data.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository {
-  Order save(Order order);
-  Long count();
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  List<Order> findAllByUserId(Long userId);
 }
