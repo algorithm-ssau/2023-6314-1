548ba72ae4204681b78fe55b111bb633c441b5c1
evnikitin <yevgeniy_n12@mail.ru>
2023-03-06 13:01:58 +0400

    Removed page reload when clicking on links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter,Link, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div >
         <header>
-          <a href="/">Online Shop</a>
+          <Link to="/">Online Shop</Link>
         </header>
         <main>
           <Routes>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,18 @@
 import data from "../data";
+import {Link} from 'react-router-dom';
 const HomeScreen = ()=>{
    return <div>
       <h1>Featured Products</h1>
           <div className="products">
             {data.products.map((product) => (
               <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
+                <Link to={`/product/${product.slug}`}>
                   <img src={product.image} alt={product.name} />
-                </a>
+                </Link>
                 <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
+                  <Link to={`/product/${product.slug}`}>
                     <p>{product.name}</p>
-                  </a>
+                  </Link>
                   <p>
                     <strong>${product.price}</strong>
                   </p>
