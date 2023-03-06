0f80b8efa24b7e8ab04f4608701b8d003c30a7e4
satishusk <sanpavkaz@gmail.com>
2023-03-06 22:20:47 +0400

    Added order api

---
+++ backend/store/order-service/src/main/java/com/team/orderservice/rest/OrderController.java
@@ -0,0 +1,64 @@
+package com.team.orderservice.rest;
+
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
+import com.team.orderservice.data.Order;
+import com.team.orderservice.dto.OrderRequestDto;
+import com.team.orderservice.dto.OrderResponseDto;
+import com.team.orderservice.dto.SummaryDto;
+import com.team.orderservice.mapper.impl.OrderRequestDtoMapper;
+import com.team.orderservice.mapper.impl.OrderResponseDtoMapper;
+import com.team.orderservice.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+  private final OrderService orderService;
+  private final OrderRequestDtoMapper orderRequestDtoMapper;
+  private final OrderResponseDtoMapper orderResponseDtoMapper;
+
+  @GetMapping
+  public ResponseEntity<List<OrderResponseDto>> getAll() {
+    List<OrderResponseDto> orderResponseDtos = orderService.getAll().stream()
+      .map(orderResponseDtoMapper::map).toList();
+    return ResponseEntity.ok().body(orderResponseDtos);
+  }
+
+  @PostMapping
+  public ResponseEntity<OrderResponseDto> create(@Valid @RequestBody OrderRequestDto orderRequestDto) {
+    Order order = orderRequestDtoMapper.map(orderRequestDto);
+    orderService.create(order);
+    return ResponseEntity.ok().build();
+  }
+
+  //TODO
+  @GetMapping("/summary")
+  public ResponseEntity<List<SummaryDto>> summary() {
+    return ResponseEntity.ok().body(List.of(new SummaryDto()));
+  }
+
+  //TODO
+  @GetMapping("/mine")
+  public ResponseEntity<List<OrderResponseDto>> getAllByUserId() {
+    return getAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<OrderResponseDto> getById(@PathVariable Long id) {
+    Order order = orderService.getById(id);
+    OrderResponseDto orderResponseDto = orderResponseDtoMapper.map(order);
+    return ResponseEntity.ok().body(orderResponseDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<OrderResponseDto> deleteById(@PathVariable Long id) {
+    orderService.deleteById(id);
+    return ResponseEntity.ok().build();
+  }
+}
