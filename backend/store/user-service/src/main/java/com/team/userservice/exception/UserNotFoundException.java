277a302a0ae697e75d41f7b4b7ef566124a9de96
satishusk <sanpavkaz@gmail.com>
2023-03-06 10:10:11 +0400

    Added user not found exception

---
+++ backend/store/user-service/src/main/java/com/team/userservice/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.team.userservice.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException() {
+    super();
+  }
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
