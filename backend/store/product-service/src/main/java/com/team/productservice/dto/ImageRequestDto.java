e33eb6fa82b94ac6e482f53499fd8e4285f70931
satishusk <sanpavkaz@gmail.com>
2023-03-06 14:59:28 +0400

    Added dtos

---
+++ backend/store/product-service/src/main/java/com/team/productservice/dto/ImageRequestDto.java
@@ -0,0 +1,18 @@
+package com.team.productservice.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageRequestDto {
+  @NotNull
+  private byte[] content;
+}
+
--- backend/store/product-service/src/main/java/com/team/productservice/dto/ProductRequestDto.java
+++ backend/store/product-service/src/main/java/com/team/productservice/dto/ProductRequestDto.java
@@ -1,3 +1,28 @@
 package com.team.productservice.dto;
 
-public class ProductRequestDto { }
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequestDto {
+  @NotBlank
+  private String name;
+
+  @NotBlank
+  private String description;
+
+  @Positive
+  private Double cost;
+
+  @Positive
+  private Long countInStock;
+
+  @Size(min = 1, max = 20)
+  private List<ImageRequestDto> images;
+}
