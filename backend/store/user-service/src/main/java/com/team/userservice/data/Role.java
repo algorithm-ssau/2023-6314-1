8e7d2620a3fb40c9d09317f09df9aee4b34188f3
satishusk <sanpavkaz@gmail.com>
2023-03-06 10:39:05 +0400

    Added JSON representation for Roles

--- backend/store/user-service/src/main/java/com/team/userservice/data/Role.java
+++ backend/store/user-service/src/main/java/com/team/userservice/data/Role.java
@@ -1,9 +1,16 @@
 package com.team.userservice.data;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.team.userservice.exception.RoleNotFoundException;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @NoArgsConstructor
+@Getter
 public enum Role {
   USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
 
@@ -14,7 +21,27 @@ public enum Role {
     this.name = name;
   }
 
+  private static final Map<String, Role> jsonNamesMap = Map.of(
+    "user", USER,
+    "admin", ADMIN
+  );
+
+  @JsonCreator
+  public static Role forValue(String name) {
+    Role role = jsonNamesMap.get(name);
+    if (role != null) {
+      return role;
+    }
+    throw new RoleNotFoundException("Role type: " + name + " is not supported");
+  }
+
+  @JsonValue
   public String toValue() {
-    return name;
+    for (Map.Entry<String, Role> entry : jsonNamesMap.entrySet()) {
+      if (entry.getValue() == this) {
+        return entry.getKey();
+      }
+    }
+    throw new RoleNotFoundException("Role type: " + this + " is not supported");
   }
 }
