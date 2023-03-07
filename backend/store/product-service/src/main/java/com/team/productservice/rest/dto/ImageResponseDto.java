7aad24b63bceee286d2fac208329502189664a43
satishusk <sanpavkaz@gmail.com>
2023-03-07 18:32:26 +0400

    Added image-service client

---
+++ backend/store/product-service/src/main/java/com/team/productservice/rest/dto/ImageResponseDto.java
@@ -0,0 +1,16 @@
+package com.team.productservice.rest.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageResponseDto {
+  private Long id;
+
+  @NotNull
+  private byte[] content;
+}
