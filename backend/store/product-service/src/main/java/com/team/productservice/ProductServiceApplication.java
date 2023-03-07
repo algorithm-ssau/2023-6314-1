b07ad73d054c0918be24816b6c3ca5cbc7084565
satishusk <sanpavkaz@gmail.com>
2023-03-07 18:47:31 +0400

    Added feign clients

--- backend/store/product-service/src/main/java/com/team/productservice/ProductServiceApplication.java
+++ backend/store/product-service/src/main/java/com/team/productservice/ProductServiceApplication.java
@@ -2,11 +2,12 @@ package com.team.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProductServiceApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
