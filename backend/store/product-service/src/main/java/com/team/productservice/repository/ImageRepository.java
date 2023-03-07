9b3709e4aab95efa5be6692abf891f5f7c782944
satishusk <sanpavkaz@gmail.com>
2023-03-07 18:26:36 +0400

    Add image repository

--- backend/store/product-service/src/main/java/com/team/productservice/repository/ImageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.team.productservice.repository;
-
-import com.team.productservice.data.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> { }
