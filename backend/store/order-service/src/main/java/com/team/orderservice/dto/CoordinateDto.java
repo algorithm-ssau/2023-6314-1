6ad935073849281ffe6575b61d15f4c61082ee60
satishusk <sanpavkaz@gmail.com>
2023-03-18 03:49:07 +0400

    Added order dto and mapper

---
+++ backend/store/order-service/src/main/java/com/team/orderservice/dto/CoordinateDto.java
@@ -0,0 +1,51 @@
+package com.team.orderservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Value;
+
+public enum CoordinateDto {;
+  private interface Latitude {
+    @Pattern(regexp = "^-?([0-8]?[0-9]|90)(\\.[0-9]{1,40})?$")
+    String getLatitude();
+  }
+  private interface Longitude {
+    @Pattern(regexp = "^-?([0-9]{1,2}|1[0-7][0-9]|180)(\\.[0-9]{1,40})?$")
+    String getLongitude();
+  }
+
+  public enum Request {;
+    @Value
+    public static class Common implements Latitude, Longitude {
+      String latitude;
+      String longitude;
+
+      @JsonCreator
+      public Common(
+        @JsonProperty("latitude") String latitude,
+        @JsonProperty("longitude") String longitude
+      ) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+      }
+    }
+  }
+
+  public enum Response {;
+    @Value
+    public static class Common implements Latitude, Longitude {
+      String latitude;
+      String longitude;
+
+      @JsonCreator
+      public Common(
+        @JsonProperty("latitude") String latitude,
+        @JsonProperty("longitude") String longitude
+      ) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+      }
+    }
+  }
+}
