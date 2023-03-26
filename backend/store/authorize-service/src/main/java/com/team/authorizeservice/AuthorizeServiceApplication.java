4720a714b7ad0282b7cb4f7aac1bbd74ae41aea5
satishusk <sanpavkaz@gmail.com>
2023-03-26 22:52:09 +0400

    Disable feign client, add jjwt

--- backend/store/authorize-service/src/main/java/com/team/authorizeservice/AuthorizeServiceApplication.java
+++ backend/store/authorize-service/src/main/java/com/team/authorizeservice/AuthorizeServiceApplication.java
@@ -3,15 +3,11 @@ package com.team.authorizeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class AuthorizeServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizeServiceApplication.class, args);
 	}
-
 }
