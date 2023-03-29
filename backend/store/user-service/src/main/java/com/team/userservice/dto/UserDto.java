3eddf2a4f209316e02acf80d3ec29d574f7474e0
satishusk <sanpavkaz@gmail.com>
2023-03-29 20:34:15 +0400

    Added timestamp for user dto

--- backend/store/user-service/src/main/java/com/team/userservice/dto/UserDto.java
+++ backend/store/user-service/src/main/java/com/team/userservice/dto/UserDto.java
@@ -2,11 +2,10 @@ package com.team.userservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Value;
+
+import java.time.OffsetDateTime;
 
 public enum UserDto {;
   private interface Id { @NotNull Long getId(); }
@@ -15,15 +14,21 @@ public enum UserDto {;
   private interface Password { @Size(min = 5, max = 80) String getPassword(); }
   private interface Active { @NotNull Boolean getActive(); }
   private interface Role { @NotNull RoleDto getRole(); }
+  private interface CreatedDateTime { @PastOrPresent OffsetDateTime getCreatedDateTime(); }
+  private interface UpdatedDateTime { @PastOrPresent OffsetDateTime getUpdatedDateTime(); }
 
   public enum Request {;
     @Value
-    public static class Common implements Name, EmailAddress, Password, Active, Role {
+    public static class Common implements
+      Name, EmailAddress, Password, Active, Role, CreatedDateTime, UpdatedDateTime
+    {
       String name;
       String email;
       String password;
       Boolean active;
       RoleDto role;
+      OffsetDateTime createdDateTime;
+      OffsetDateTime updatedDateTime;
 
       @JsonCreator
       public Common(
@@ -31,25 +36,33 @@ public enum UserDto {;
         @JsonProperty("email") String email,
         @JsonProperty("password") String password,
         @JsonProperty("active") Boolean active,
-        @JsonProperty("role") RoleDto role
+        @JsonProperty("role") RoleDto role,
+        @JsonProperty("createdDateTime") OffsetDateTime createdDateTime,
+        @JsonProperty("updatedDateTime") OffsetDateTime updatedDateTime
       ) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.active = active;
         this.role = role;
+        this.createdDateTime = createdDateTime;
+        this.updatedDateTime = updatedDateTime;
       }
     }
   }
 
   public enum Response {;
     @Value
-    public static class Common implements Id, Name, EmailAddress, Active, Role {
+    public static class Common implements
+      Id, Name, EmailAddress, Active, Role, CreatedDateTime, UpdatedDateTime
+    {
       Long id;
       String name;
       String email;
       Boolean active;
       RoleDto role;
+      OffsetDateTime createdDateTime;
+      OffsetDateTime updatedDateTime;
 
       @JsonCreator
       public Common(
@@ -57,13 +70,38 @@ public enum UserDto {;
         @JsonProperty("name") String name,
         @JsonProperty("email") String email,
         @JsonProperty("active") Boolean active,
-        @JsonProperty("role") RoleDto role
+        @JsonProperty("role") RoleDto role,
+        @JsonProperty("createdDateTime") OffsetDateTime createdDateTime,
+        @JsonProperty("updatedDateTime") OffsetDateTime updatedDateTime
       ) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.active = active;
         this.role = role;
+        this.createdDateTime = createdDateTime;
+        this.updatedDateTime = updatedDateTime;
+      }
+    }
+
+    @Value
+    public static class Activation implements Id, Active, EmailAddress, CreatedDateTime {
+      Long id;
+      Boolean active;
+      String email;
+      OffsetDateTime createdDateTime;
+
+      @JsonCreator
+      public Activation(
+        @JsonProperty("id") Long id,
+        @JsonProperty("email") String email,
+        @JsonProperty("active") Boolean active,
+        @JsonProperty("createdDateTime") OffsetDateTime createdDateTime
+      ) {
+        this.id = id;
+        this.active = active;
+        this.email = email;
+        this.createdDateTime = createdDateTime;
       }
     }
   }
