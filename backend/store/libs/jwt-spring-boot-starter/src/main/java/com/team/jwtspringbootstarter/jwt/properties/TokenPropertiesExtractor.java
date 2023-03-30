5464be67961106c48c83a98d9288bab0fb6b364e
satishusk <sanpavkaz@gmail.com>
2023-03-30 19:04:32 +0400

    Set default token properties

--- backend/store/libs/jwt-spring-boot-starter/src/main/java/com/team/jwtspringbootstarter/jwt/properties/TokenPropertiesExtractor.java
+++ backend/store/libs/jwt-spring-boot-starter/src/main/java/com/team/jwtspringbootstarter/jwt/properties/TokenPropertiesExtractor.java
@@ -11,19 +11,19 @@ import java.security.Key;
 
 @Slf4j
 public class TokenPropertiesExtractor {
-  @Value("${jwt.token.access.header}")
+  @Value("${jwt.token.access.header:${jwt.token.access.header.default}}")
   private String header;
 
-  @Value("${jwt.token.access.secret}")
+  @Value("${jwt.token.access.secret:${jwt.token.access.secret.default}}")
   private String accessSecret;
 
-  @Value("${jwt.token.access.expired}")
+  @Value("${jwt.token.access.expired:${jwt.token.access.expired.default}}")
   private long accessValidityInMilliseconds;
 
   @PostConstruct
   private void init() {
     accessSecret = Encoders.BASE64.encode(accessSecret.getBytes());
-    log.debug("Encoded access and refresh secret key");
+    log.debug("Encoded access secret key");
   }
 
   @lombok.Value
@@ -34,14 +34,10 @@ public class TokenPropertiesExtractor {
   }
 
   public TokenData pullAccessTokenData() {
-    return generateTokenData(accessSecret, accessValidityInMilliseconds);
-  }
-
-  private TokenData generateTokenData(String secret, long validityInMilliseconds) {
     return new TokenData(
       header,
-      Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret)),
-      validityInMilliseconds
+      Keys.hmacShaKeyFor(Decoders.BASE64.decode(accessSecret)),
+      accessValidityInMilliseconds
     );
   }
 }
