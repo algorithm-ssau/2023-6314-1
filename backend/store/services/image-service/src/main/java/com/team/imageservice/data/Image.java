6c0ae97de85eed09188196fbbd8a5da175ce7038
satishusk <sanpavkaz@gmail.com>
2023-03-31 22:55:14 +0400

    Optimized image imports

--- backend/store/services/image-service/src/main/java/com/team/imageservice/data/Image.java
+++ backend/store/services/image-service/src/main/java/com/team/imageservice/data/Image.java
@@ -1,9 +1,6 @@
 package com.team.imageservice.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
