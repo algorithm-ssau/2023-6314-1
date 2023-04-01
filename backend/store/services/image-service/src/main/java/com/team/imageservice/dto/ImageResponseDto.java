f2ae88f906bab4db99a371f74a318ac829bd7885
satishusk <sanpavkaz@gmail.com>
2023-04-01 16:21:11 +0400

    Moved files

---
+++ backend/store/services/image-service/src/main/java/com/team/imageservice/dto/ImageResponseDto.java
@@ -0,0 +1,17 @@
+package com.team.imageservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageResponseDto {
+  @NotNull
+  private Long id;
+
+  @NotNull
+  private byte[] content;
+}
