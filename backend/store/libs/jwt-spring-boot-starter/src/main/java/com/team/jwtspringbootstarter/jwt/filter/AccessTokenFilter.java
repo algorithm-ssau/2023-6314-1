a6bf00caf1343917a1a2291890a988409b8c80ad
satishusk <sanpavkaz@gmail.com>
2023-04-01 22:26:50 +0400

    Changed interface of AccessTokenFilter

--- backend/store/libs/jwt-spring-boot-starter/src/main/java/com/team/jwtspringbootstarter/jwt/filter/AccessTokenFilter.java
+++ backend/store/libs/jwt-spring-boot-starter/src/main/java/com/team/jwtspringbootstarter/jwt/filter/AccessTokenFilter.java
@@ -1,20 +1,26 @@
 package com.team.jwtspringbootstarter.jwt.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.team.jwtspringbootstarter.jwt.authentication.JwtSecurityProvider;
+import com.team.jwtspringbootstarter.jwt.exception.JwtAuthenticationException;
+import com.team.jwtspringbootstarter.jwt.exception.JwtAuthorizeException;
+import com.team.jwtspringbootstarter.jwt.exception.TokenResolvingException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Slf4j
-public class AccessTokenFilter extends GenericFilterBean {
+public class AccessTokenFilter extends OncePerRequestFilter {
   private final JwtSecurityProvider jwtSecurityProvider;
 
   public AccessTokenFilter(JwtSecurityProvider jwtSecurityProvider) {
@@ -22,16 +28,23 @@ public class AccessTokenFilter extends GenericFilterBean {
   }
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    var token = jwtSecurityProvider.resolveToken((HttpServletRequest) request);
-    log.debug("Resolved token: {}", token);
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    try {
+      var token = jwtSecurityProvider.resolveToken(request);
+      log.debug("Resolved token: {}", token);
 
-    if (token != null && jwtSecurityProvider.validateToken(token)) {
-      Authentication authentication = jwtSecurityProvider.loadAuthenticationByToken(token);
-      var securityContext = SecurityContextHolder.getContext();
-      securityContext.setAuthentication(authentication);
-      log.debug("Authenticated: {} in context: {}", authentication, securityContext);
+      if (token != null && jwtSecurityProvider.validateToken(token)) {
+        Authentication authentication = jwtSecurityProvider.loadAuthenticationByToken(token);
+        var securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(authentication);
+        log.debug("Authenticated: {} in context: {}", authentication, securityContext);
+      } else {
+        throw new JwtAuthorizeException("Token not valid");
+      }
+      filterChain.doFilter(request, response);
+    } catch (JwtAuthorizeException | TokenResolvingException ex) {
+      response.setStatus(HttpStatus.UNAUTHORIZED.value());
+      response.getWriter().write(new ObjectMapper().writeValueAsString(ex.getMessage()));
     }
-    chain.doFilter(request, response);
   }
 }
