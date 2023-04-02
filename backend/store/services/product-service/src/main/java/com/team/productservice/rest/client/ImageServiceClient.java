9f560588607684618b3616af38e5fcd1780bc42b
satishusk <sanpavkaz@gmail.com>
2023-04-02 19:35:35 +0400

    Changed product-service image rest client req/resp image to bytes

--- backend/store/services/product-service/src/main/java/com/team/productservice/rest/client/ImageServiceClient.java
+++ backend/store/services/product-service/src/main/java/com/team/productservice/rest/client/ImageServiceClient.java
@@ -20,42 +20,25 @@ import static org.springframework.web.reactive.function.client.WebClient.*;
 @Component
 public class ImageServiceClient {
   private final WebClient client;
-  private final ObjectMapper objectMapper;
 
   @Autowired
-  public ImageServiceClient(WebClient imageServiceWebClient, ObjectMapper objectMapper) {
+  public ImageServiceClient(WebClient imageServiceWebClient) {
     this.client = imageServiceWebClient;
-    this.objectMapper = objectMapper;
   }
 
-  public ImageResponseDto get(Long id) {
-    RequestHeadersUriSpec<?> requestHeadersUriSpec = client.get();
-    RequestHeadersSpec<?> headersSpec = requestHeadersUriSpec.uri("/api/images/" + id);
-    String response = headersSpec.retrieve().bodyToMono(String.class).block();
-    return parseImageResponseDto(Objects.requireNonNull(response));
-  }
-
-  public Long save(@Valid ImageRequestDto dto) {
+  public Long save(byte[] imageBytes) {
     RequestBodyUriSpec bodyUriSpec = client.post();
     RequestHeadersSpec<?> headersSpec = bodyUriSpec.uri("/api/images")
-      .body(BodyInserters.fromValue(dto));
+      .body(BodyInserters.fromValue(imageBytes));
     Mono<Long> idMono = headersSpec.retrieve().bodyToMono(Long.class);
     return Objects.requireNonNull(idMono.block());
   }
 
-  public List<Long> saveAll(List<ImageRequestDto> imagesRequestDto) {
+  public List<Long> saveAll(byte[][] imagesBytes) {
     List<Long> imagesId = new ArrayList<>();
-    for (ImageRequestDto imageRequestDto : imagesRequestDto) {
-      imagesId.add(save(imageRequestDto));
+    for (byte[] imageBytes : imagesBytes) {
+      imagesId.add(save(imageBytes));
     }
     return imagesId;
-  }
-
-  private ImageResponseDto parseImageResponseDto(String response) {
-    try {
-      return objectMapper.readValue(response.getBytes(), ImageResponseDto.class);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 }
