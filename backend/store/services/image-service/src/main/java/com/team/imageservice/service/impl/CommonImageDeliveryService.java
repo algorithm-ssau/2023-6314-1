ba019ca27ee5fe713662ad1b30ccf91ec7021cbe
satishusk <sanpavkaz@gmail.com>
2023-04-04 15:53:17 +0400

    Refactoring CommonImageDeliveryService: changed @RequiredArgsConstructor to constructor

--- backend/store/services/image-service/src/main/java/com/team/imageservice/service/impl/CommonImageDeliveryService.java
+++ backend/store/services/image-service/src/main/java/com/team/imageservice/service/impl/CommonImageDeliveryService.java
@@ -3,13 +3,19 @@ package com.team.imageservice.service.impl;
 import com.team.imageservice.data.Image;
 import com.team.imageservice.exception.ImageNotFoundException;
 import com.team.imageservice.repository.ImageRepository;
-import lombok.RequiredArgsConstructor;
+import com.team.imageservice.service.api.ImageDeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class CommonImageDeliveryService implements ImageDeliveryService {
   private final ImageRepository imageRepository;
+
+
+  @Autowired
+  public CommonImageDeliveryService(ImageRepository imageRepository) {
+    this.imageRepository = imageRepository;
+  }
 
   public Image getById(Long id) {
     return imageRepository.findById(id)
