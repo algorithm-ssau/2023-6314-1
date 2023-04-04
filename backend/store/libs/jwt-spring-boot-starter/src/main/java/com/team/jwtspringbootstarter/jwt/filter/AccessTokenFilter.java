b59a3302f59f1cc3ae37d0c06d75aac8525dbae3
satishusk <sanpavkaz@gmail.com>
2023-04-04 07:43:48 +0400

    Added NonNull api to AccessTokenFilter

--- backend/store/libs/jwt-spring-boot-starter/src/main/java/com/team/jwtspringbootstarter/jwt/filter/AccessTokenFilter.java
+++ backend/store/libs/jwt-spring-boot-starter/src/main/java/com/team/jwtspringbootstarter/jwt/filter/AccessTokenFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,7 +22,9 @@ public class AccessTokenFilter extends OncePerRequestFilter {
   }
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+  protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                  @NonNull HttpServletResponse response,
+                                  @NonNull FilterChain filterChain) throws ServletException, IOException {
     var optionalToken = jwtSecurityProvider.resolveToken(request);
     if (optionalToken.isPresent()) {
       log.debug("Resolved optionalToken: {}", optionalToken);
