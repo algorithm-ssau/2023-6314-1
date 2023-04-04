6d83a2ce65cbe68d6c55b56f63c0898b7f689f78
satishusk <sanpavkaz@gmail.com>
2023-04-04 07:07:07 +0400

    Moved ProductNotFoundHandler

--- backend/store/services/product-service/src/main/java/com/team/productservice/rest/server/advice/ProductNotFoundHandler.java
+++ backend/store/services/product-service/src/main/java/com/team/productservice/rest/server/advice/ProductNotFoundHandler.java
@@ -1,6 +1,7 @@
 package com.team.productservice.rest.server.advice;
 
 import com.team.productservice.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ProductNotFoundHandler {
   @ExceptionHandler(ProductNotFoundException.class)
   public ResponseEntity<String> handle(ProductNotFoundException exception) {
-    return ResponseEntity.status(404).body(exception.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
   }
 }
