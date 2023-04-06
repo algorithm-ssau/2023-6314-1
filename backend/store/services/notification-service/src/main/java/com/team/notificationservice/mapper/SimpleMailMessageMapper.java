e793a655f2f1789869440982feaf045983456d70
satishusk <sanpavkaz@gmail.com>
2023-04-06 13:54:52 +0400

    Added a mapper for creating mail messages from dto

---
+++ backend/store/services/notification-service/src/main/java/com/team/notificationservice/mapper/SimpleMailMessageMapper.java
@@ -0,0 +1,33 @@
+package com.team.notificationservice.mapper;
+
+import com.team.notificationservice.dto.ActivationDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:mail.properties")
+public class SimpleMailMessageMapper {
+  @Value("${mail.username}")
+  private String from;
+
+  public SimpleMailMessage toMailMessage(ActivationDto activationDto) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(from);
+    message.setTo(activationDto.getEmail());
+    message.setSubject("Activation");
+    message.setText(buildMessageText(activationDto));
+    return message;
+  }
+
+  private String buildMessageText(ActivationDto activationDto) {
+    return "Hello " + activationDto.getName() + ',' + "\n\n" +
+      "Thank you for using our service. Your account has been successfully registered." + "\n\n" +
+      "Please click here to verify your account:" + "\n\n" +
+      activationDto.getActivationLink() + "\n\n" +
+      "This link will expire in 24 hours!" + "\n\n" +
+      "Kind regards," + "\n\n" +
+      "Your com.team.store";
+  }
+}
