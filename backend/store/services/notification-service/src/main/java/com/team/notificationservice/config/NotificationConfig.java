8122f520a5c51245291255ef5fe57581462c4a82
satishusk <sanpavkaz@gmail.com>
2023-04-06 13:53:50 +0400

    Added notification config

---
+++ backend/store/services/notification-service/src/main/java/com/team/notificationservice/config/NotificationConfig.java
@@ -0,0 +1,13 @@
+package com.team.notificationservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationConfig {
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
+  }
+}
