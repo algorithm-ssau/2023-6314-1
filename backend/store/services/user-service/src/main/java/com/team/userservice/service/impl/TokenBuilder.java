54d3c6ebef9638bf0428912b8d5ffdc21e4e3c08
satishusk <sanpavkaz@gmail.com>
2023-04-10 02:50:48 +0400

    Added activation token builder

---
+++ backend/store/services/user-service/src/main/java/com/team/userservice/service/impl/TokenBuilder.java
@@ -0,0 +1,42 @@
+package com.team.userservice.service.impl;
+
+import com.team.userservice.data.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@Slf4j
+@PropertySource("classpath:jwt.properties")
+public class TokenBuilder {
+  private final TokenConfig.TokenData activateTokenData;
+
+  @Autowired
+  public TokenBuilder(@Qualifier("activateTokenData") TokenConfig.TokenData activateTokenData) {
+    this.activateTokenData = activateTokenData;
+  }
+
+  public String buildTokenBody(User user) {
+    Date currentDate = new Date();
+    Date expiredDate = new Date(currentDate.getTime() + activateTokenData.getValidityDateInMilliseconds());
+
+    return Jwts.builder()
+      .setClaims(buildJwtClaims(user.getEmail()))
+      .setIssuedAt(currentDate)
+      .setExpiration(expiredDate)
+      .signWith(activateTokenData.getSecretKey())
+      .compact();
+  }
+
+  private Claims buildJwtClaims(String email) {
+    var claims = Jwts.claims();
+    claims.setSubject(email);
+    return claims;
+  }
+}
