8e9075723e45a65a1420c3cdaf8c1666f2d59122
satishusk <sanpavkaz@gmail.com>
2023-04-17 09:32:21 +0400

    Removed logging from filter

--- backend/store/libs/jwt-common/src/main/java/com/team/jwtcommon/filter/AccessTokenFilter.java
+++ backend/store/libs/jwt-common/src/main/java/com/team/jwtcommon/filter/AccessTokenFilter.java
@@ -3,12 +3,10 @@ package com.team.jwtcommon.filter;
 import com.team.basejwt.filter.AbstractTokenFilter;
 import com.team.basejwt.properties.TokenMetadata;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 
 import java.util.Optional;
 
-@Slf4j
 public class AccessTokenFilter extends AbstractTokenFilter {
 
   public AccessTokenFilter(AuthenticationManager authenticationManager, TokenMetadata accessTokenMetadata) {
