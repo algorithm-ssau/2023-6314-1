d40b45b7b2ba63ba41b23d3ded87d2f5e74aa544
evnikitin <yevgeniy_n12@mail.ru>
2023-04-23 19:00:25 +0400

    created Product component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import {useState,useEffect,useReducer} from "react" ;
 import {Link} from 'react-router-dom'
 import axios from "axios";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
 import data from '../data';
 
 const reducer = (state, action) => {
@@ -47,22 +50,13 @@ const HomeScreen=()=>{
         ) : error ? (
           <div>{error}</div>
         ) : (          
-          products.map((product) => (
-            <div className="product" key={product.id}>
-              <Link to={`/product/${product.id}`}>
-                <img src={product.imagesContent[0]} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.id}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.cost}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          )))}
+          <Row>
+            {products.map((product) => (
+              <Col key={product.id} sm={6} md={5} lg={4} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>)}
         </div>
 
    </>)
