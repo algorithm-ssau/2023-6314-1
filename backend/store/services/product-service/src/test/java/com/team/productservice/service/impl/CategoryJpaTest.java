c0095991839559179b53d811152f804d4ffa544f
satishusk <sanpavkaz@gmail.com>
2023-04-29 15:51:36 +0400

    Added category jpa test

---
+++ backend/store/services/product-service/src/test/java/com/team/productservice/service/impl/CategoryJpaTest.java
@@ -0,0 +1,64 @@
+package com.team.productservice.service.impl;
+
+import com.team.productservice.data.Category;
+import com.team.productservice.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CategoryJpaTest {
+  @Autowired
+  private CategoryRepository categoryRepository;
+
+  private static final List<Category> rootCategories = new ArrayList<>();
+
+  @BeforeEach
+  void beforeEach() {
+    Category electronicsCategory = new Category("Electronics");
+    electronicsCategory.addSub("Smartphones");
+    electronicsCategory.addSub("Blenders");
+
+    Category clothesCategory = new Category("Clothes");
+    clothesCategory.addSub("Shoes");
+    clothesCategory.addSub("Outerwear");
+
+    Category sportingGoodsCategory = new Category("Sporting goods");
+    sportingGoodsCategory.addSub("Bicycles");
+    sportingGoodsCategory.addSub("Simulators");
+    sportingGoodsCategory.addSub("Balls");
+
+    rootCategories.add(electronicsCategory);
+    rootCategories.add(clothesCategory);
+    rootCategories.add(sportingGoodsCategory);
+  }
+
+  @Test
+  void shouldAdd() {
+    List<Category> savedCategories = categoryRepository.saveAll(rootCategories);
+    Category anyCategory = savedCategories.get(0);
+    Category anySubCategory = anyCategory.getSubCategories().stream().toList().get(0);
+    assertTrue(() -> anyCategory.getSubCategories().contains(anySubCategory));
+  }
+
+  @Test
+  void shouldReturnValidProjection() {
+    Set<Long> ids = categoryRepository.saveAll(rootCategories).stream()
+      .map(Category::getId)
+      .collect(Collectors.toSet());
+    Set<Category> projection = categoryRepository.findProjection(ids);
+
+    assertIterableEquals(ids, projection.stream().map(Category::getId).toList());
+  }
+}
