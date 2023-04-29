2ac9ccfb60fa353c2717585092eb900c8d7c704b
satishusk <sanpavkaz@gmail.com>
2023-04-29 15:54:04 +0400

    Added category dto to product dto

--- backend/store/services/product-service/src/main/java/com/team/productservice/dto/ProductDto.java
+++ backend/store/services/product-service/src/main/java/com/team/productservice/dto/ProductDto.java
@@ -18,15 +18,20 @@ public enum ProductDto {;
   private interface CountInStock { @Positive Long getCountInStock(); }
   private interface ImagesContent { @NotNull List<String> getImagesContent(); }
   private interface ImagesBytes { @NotNull byte[][] getImagesBytes(); }
+  private interface CategoryRequest { @NotNull CategoryDto.Request.Common getCategory(); }
+  private interface CategoryResponse { @NotNull CategoryDto.Response.Common getCategory(); }
 
   public enum Request {;
     @Value
-    public static class Common implements Name, Description, Cost, CountInStock, ImagesContent {
+    public static class Common implements
+      Name, Description, Cost, CountInStock, ImagesContent, CategoryRequest
+    {
       String name;
       String description;
       BigDecimal cost;
       Long countInStock;
       List<String> imagesContent;
+      CategoryDto.Request.Common category;
 
       @JsonCreator
       public Common(
@@ -34,23 +39,26 @@ public enum ProductDto {;
         @JsonProperty("description") String description,
         @JsonProperty("cost") BigDecimal cost,
         @JsonProperty("countInStock") Long countInStock,
-        @JsonProperty("imagesContent") List<String> imagesContent
+        @JsonProperty("imagesContent") List<String> imagesContent,
+        @JsonProperty("category") CategoryDto.Request.Common category
       ) {
         this.name = name;
         this.description = description;
         this.cost = cost;
         this.countInStock = countInStock;
         this.imagesContent = imagesContent;
+        this.category = category;
       }
     }
 
     @Value
-    public static class Create implements Name, Description, Cost, CountInStock, ImagesBytes {
+    public static class Create implements Name, Description, Cost, CountInStock, ImagesBytes, CategoryRequest {
       String name;
       String description;
       BigDecimal cost;
       Long countInStock;
       byte[][] imagesBytes;
+      CategoryDto.Request.Common category;
 
       @JsonCreator
       public Create(
@@ -58,23 +66,26 @@ public enum ProductDto {;
         @JsonProperty("description") String description,
         @JsonProperty("cost") BigDecimal cost,
         @JsonProperty("countInStock") Long countInStock,
-        @JsonProperty("imagesBytes") byte[][] imagesBytes
+        @JsonProperty("imagesBytes") byte[][] imagesBytes,
+        @JsonProperty("category") CategoryDto.Request.Common category
       ) {
         this.name = name;
         this.description = description;
         this.cost = cost;
         this.countInStock = countInStock;
         this.imagesBytes = imagesBytes;
+        this.category = category;
       }
     }
 
     @Value
-    public static class Update implements Name, Description, Cost, CountInStock, ImagesBytes {
+    public static class Update implements Name, Description, Cost, CountInStock, ImagesBytes, CategoryRequest {
       String name;
       String description;
       BigDecimal cost;
       Long countInStock;
       byte[][] imagesBytes;
+      CategoryDto.Request.Common category;
 
       @JsonCreator
       public Update(
@@ -82,26 +93,29 @@ public enum ProductDto {;
         @JsonProperty("description") String description,
         @JsonProperty("cost") BigDecimal cost,
         @JsonProperty("countInStock") Long countInStock,
-        @JsonProperty("imagesBytes") byte[][] imagesBytes
+        @JsonProperty("imagesBytes") byte[][] imagesBytes,
+        @JsonProperty("category") CategoryDto.Request.Common category
       ) {
         this.name = name;
         this.description = description;
         this.cost = cost;
         this.countInStock = countInStock;
         this.imagesBytes = imagesBytes;
+        this.category = category;
       }
     }
   }
 
   public enum Response {;
     @Value
-    public static class Common implements Id, Name, Description, Cost, CountInStock, ImagesContent {
+    public static class Common implements Id, Name, Description, Cost, CountInStock, ImagesContent, CategoryResponse {
       Long id;
       String name;
       String description;
       BigDecimal cost;
       Long countInStock;
       List<String> imagesContent;
+      CategoryDto.Response.Common category;
 
       @JsonCreator
       public Common(
@@ -110,7 +124,8 @@ public enum ProductDto {;
         @JsonProperty("description") String description,
         @JsonProperty("cost") BigDecimal cost,
         @JsonProperty("countInStock") Long countInStock,
-        @JsonProperty("imagesContent") List<String> imagesContent
+        @JsonProperty("imagesContent") List<String> imagesContent,
+        @JsonProperty("category") CategoryDto.Response.Common category
       ) {
         this.id = id;
         this.name = name;
@@ -118,6 +133,7 @@ public enum ProductDto {;
         this.cost = cost;
         this.countInStock = countInStock;
         this.imagesContent = imagesContent;
+        this.category = category;
       }
     }
   }
