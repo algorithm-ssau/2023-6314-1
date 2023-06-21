ec1eefc28bfa321ba3320ec6dbf717d52bd0929a
satishusk <sanpavkaz@gmail.com>
2023-06-22 00:00:09 +0400

    Added equals and hashcode to User

--- backend/store/services/user-service/src/main/java/com/team/userservice/model/User.java
+++ backend/store/services/user-service/src/main/java/com/team/userservice/model/User.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -59,5 +60,18 @@ public class User {
     this.password = password;
     this.role = role;
     this.active = active;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && role == user.role && Objects.equals(active, user.active);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, email, role, active);
   }
 }
