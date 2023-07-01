30bdf55ae87698935817edb3b78b4f8096a3dafe
satishusk <sanpavkaz@gmail.com>
2023-07-02 02:14:26 +0400

    Removed CORS configs from base security configs

--- backend/store/libs/security/src/main/java/com/team/security/config/JwtFilterSecurityConfigurer.java
+++ backend/store/libs/security/src/main/java/com/team/security/config/JwtFilterSecurityConfigurer.java
@@ -1,8 +1,6 @@
 package com.team.security.config;
 
 import com.team.jwt.filter.AbstractTokenFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -10,7 +8,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
 
 @Component
 public class JwtFilterSecurityConfigurer implements SecurityConfigurer {
@@ -20,7 +17,6 @@ public class JwtFilterSecurityConfigurer implements SecurityConfigurer {
                                          AbstractTokenFilter tokenFilter,
                                          Customizer<AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry> customizer) throws Exception {
     return http
-      .cors().configurationSource(this::corsConfigurationSource).and()
       .csrf().disable()
       .httpBasic().disable()
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
@@ -30,13 +26,6 @@ public class JwtFilterSecurityConfigurer implements SecurityConfigurer {
       })
       .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
       .build();
-  }
-
-  private CorsConfiguration corsConfigurationSource(HttpServletRequest request) {
-    CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
-    corsConfiguration.addAllowedMethod(HttpMethod.PUT);
-    corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
-    return corsConfiguration;
   }
 
   private void authorizeHttpRequestsCustomizer(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
